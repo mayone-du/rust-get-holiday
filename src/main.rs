cb71d7513fb58d31f85762a5a73890889451f5d9
mayone-du <cocomayo1201@icloud.com>
2021-09-30 09:29:44 +0900

    Add koyomi, print calendar

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use chrono::{Datelike, Local};
+use chrono::{Datelike, Local, NaiveDate};
+use koyomi::{Calendar, Date};
 
 fn main() {
     // 実行した日時、時刻を取得
     let now = Local::now();
+    println!("now {}", &now);
 
     // 実行したタイミングの月を取得
     let month = now.month();
@@ -20,7 +22,21 @@ fn main() {
     };
     println!("{}", weekday);
 
+    // 開始日時
+    let start_date = NaiveDate::from_ymd(now.year(), now.month(), 1);
+    println!("start_date {}", start_date);
+
+    let date = Date::parse("2021-09-01").unwrap();
+    let date2 = Date::parse("2021-09-30").unwrap();
+
+    let calendar = Calendar::new(date, date2).unwrap().make();
+    for day in calendar {
+        println!("calendar days {} {}", day.day(), day.weekday().japanese(),);
+    }
+
     // 現在日時をフォーマット
     let now = now.format("%Y年 %m月 %d日");
     println!("now -> {}", now);
 }
+
+// TODO: test
