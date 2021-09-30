b4083f3174d6d554a49679a55f144c8f12ecf77b
mayone-du <cocomayo1201@icloud.com>
2021-09-30 11:22:02 +0900

    Add print holidays

--- src/main.rs
+++ src/main.rs
@@ -25,14 +25,26 @@ fn main() {
     // カレンダーを取得
     let calendar = Calendar::new(start_date, end_date).unwrap().make();
     for day in calendar {
-        println!(
-            "calendar days {} {} {:?}",
-            day.day(),
-            // 曜日
-            day.weekday().japanese(),
-            // 祝日
-            day.holiday(),
-        );
+        // 全日にちを出力
+        // println!(
+        //     "calendar days {} {} {:?}",
+        //     day.day(),
+        //     // 曜日
+        //     day.weekday().japanese(),
+        //     // 祝日
+        //     day.holiday(),
+        // );
+
+        // 土日の場合
+        match day.weekday().japanese() {
+            '土' => println!("土曜 {}", day),
+            '日' => println!("日曜 {}", day),
+            // 土日以外
+            _ => {}
+        }
+        if day.holiday() != None {
+            println!("{} {}", day.holiday().unwrap(), day);
+        }
     }
 
     // 現在日時をフォーマット
