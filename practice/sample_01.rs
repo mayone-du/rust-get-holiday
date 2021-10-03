b2759d1965db1c7fb50aa405c2c9bc26a5c7bec1
mayone-du <cocomayo1201@icloud.com>
2021-10-03 19:20:27 +0900

    Refactoring

--- practice/sample_01.rs
+++ practice/sample_01.rs
@@ -1,5 +1,7 @@
 use chrono::Local;
+use std::env::args;
 
+// 練習してただけのコード↓
 fn main() {
   // 実行した日時、時刻を取得
   let now = Local::now();
@@ -24,4 +26,25 @@ fn main() {
   // 現在日時をフォーマット
   let now = now.format("%Y年 %m月 %d日");
   println!("now -> {}", now);
+}
+
+// その月の日付をすべて出力
+fn _print_one_month_days(calendar: std::vec::Vec<koyomi::Date>) {
+  // コマンドライン引数にallがあれば、全日付を出力
+  // TODO: エラーハンドリング
+  let args: Vec<String> = args().collect();
+  let flag = &args[1];
+  if flag != "all" {}
+  print!("\n");
+  for day in calendar {
+    // 全日にちを出力
+    println!(
+      "calendar days {} {} {:?}",
+      day.day(),
+      // 曜日
+      day.weekday().japanese(),
+      // 祝日
+      day.holiday(),
+    );
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,78 @@
 use chrono::{Datelike, Local};
 use colored::Colorize;
 use koyomi::{num_days, Calendar, Date};
-use std::env::args;
+
+// カレンダー作成時に指定する、開始日と終了日をもつ期間という意味の構造体を定義
+struct Period {
+    start_date: Date,
+    end_date: Date,
+}
+
+// Period構造体に自身を作成するnewという関連関数を定義
+impl Period {
+    fn new() -> Period {
+        // 現在の日時を取得
+        let now = Local::now();
+        let (year, month) = (now.year(), now.month());
+
+        // 実行した月を対象とする 2021-10-01 と 2021-10-31 のような形式になる
+        let start_date: String = format!("{}-{}-01", year.to_string(), month.to_string(),);
+        let end_date: String = format!(
+            "{}-{}-{}",
+            year.to_string(),
+            month.to_string(),
+            // 指定した年月の最終日を返す
+            num_days(year, month)
+        );
+
+        // 日付の文字列をDate型に変換
+        let start_date = Date::parse(&start_date).unwrap();
+        let end_date = Date::parse(&end_date).unwrap();
+
+        // 自身を返却
+        Period {
+            start_date,
+            end_date,
+        }
+    }
+}
 
 fn main() {
     // 実行した日時、時刻を取得
-    let now = Local::now();
+    let period = Period::new();
 
-    let year = now.year();
-    let month = now.month();
-
-    // 実行した月を対象とする
-    let start_date: String = format!("{}-{}-01", year.to_string(), month.to_string(),);
-    let end_date: String = format!(
-        "{}-{}-{}",
-        year.to_string(),
-        month.to_string(),
-        // 指定した年月の最終日を返す
-        num_days(year, month)
-    );
-
-    // 開始日時
-    // let start_date = match Date::parse(&start_date) {
-    //     Ok(date) => date,
-    //     Err(err) => panic!("エラー {:?}", err),
-    // };
-    let start_date = Date::parse(&start_date).unwrap();
-    let end_date = Date::parse(&end_date).unwrap();
-
-    // カレンダーを取得
-    let calendar = match Calendar::new(start_date, end_date) {
+    // カレンダーを実行した月の期間で作成
+    let calendar = match Calendar::new(period.start_date, period.end_date) {
         Ok(calendar) => calendar.make(),
-        Err(err) => panic!("カレンダーエラー {:?}", err),
+        Err(err) => panic!("カレンダー初期化エラー {:?}", err),
     };
+
+    // calendarはIterableなのでforで回し、日付を受け取るj
     for day in &calendar {
-        // 土日の場合
+        // 曜日を判定し、土日の場合は赤・青で出力し、それ以外はスルー
+        // day.weekday().japanese()はchar型を返すため、''で判定
         match day.weekday().japanese() {
             '土' => println!("{} 土", day.to_string().red()),
             '日' => println!("{} 日", day.to_string().blue()),
-            // 土日以外
+            // _は、上記以外の全てという意味のワイルドカード
             _ => {}
         }
+        // 祝日の場合は、祝日の内容を緑で出力
         if day.holiday() != None {
             println!("{} {}", day.to_string().green(), day.holiday().unwrap(),);
         }
-    }
 
-    // コマンドライン引数にallがあれば、全日付を出力
-    // TODO: エラーハンドリング
-    let args: Vec<String> = args().collect();
-    let flag = &args[1];
-    if flag == "all" {
-        print_one_month_days(calendar);
+        // TODO: 土日と祝日がかぶっている場合の対処
     }
 }
 
-fn print_one_month_days(calendar: std::vec::Vec<koyomi::Date>) {
-    print!("\n");
-    for day in calendar {
-        // 全日にちを出力
-        println!(
-            "calendar days {} {} {:?}",
-            day.day(),
-            // 曜日
-            day.weekday().japanese(),
-            // 祝日
-            day.holiday(),
-        );
-    }
-}
+// TODO: テストコードの追加
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-// TODO: test
-#[cfg(test)]
-mod tests {
-    // use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    // #[test]
-    // fn it_print_one_month_days() {
-    //     // すべての日数を表示
-    //     print_one_month_days(
-    //         Calendar::new(
-    //             koyomi::Date::parse("2021-09-01").unwrap(),
-    //             koyomi::Date::parse("2021-09-30").unwrap(),
-    //         )
-    //         .unwrap()
-    //         .make(),
-    //     )
-    // }
-}
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
